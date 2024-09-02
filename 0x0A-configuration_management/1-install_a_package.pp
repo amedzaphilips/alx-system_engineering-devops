fa966169b887a30da5e79ea03f8731b956c045a1
amedzaphilips <danielamedzaphilips@gmail.com>
2024-09-02 13:01:52 -0700

    new again

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 
 # Install Flask and its dependencies using pip
 exec { 'install_flask_and_dependencies':
-  command => 'pip3 install flask==2.1.0',
+  command => 'pip3 install flask==2.1.0 werkzeug==2.0.3',
   path    => ['/usr/bin', '/usr/local/bin'],
   unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
   require => Package['python3-pip'],
